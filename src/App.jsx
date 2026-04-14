b31b912b7c25def4b83fbdec9ec83ea1244394b6
joao.prata <joao.prata@moneyprestador.com.br>
2026-04-13 23:56:27 -0300

    Adjustments

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Hero from './components/Hero'
 import Stats from './components/Stats'
 import Services from './components/Services'
 import HowItWorks from './components/HowItWorks'
-import Testimonials from './components/Testimonials'
 import CTA from './components/CTA'
 import Footer from './components/Footer'
 import WhatsAppFloat from './components/WhatsAppFloat'
@@ -18,7 +17,6 @@ export default function App() {
       <Stats />
       <Services />
       <HowItWorks />
-      <Testimonials />
       <CTA />
       <Footer />
       <WhatsAppFloat />
